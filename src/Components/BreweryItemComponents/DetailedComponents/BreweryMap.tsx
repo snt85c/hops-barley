0a23bf408d06f83dd91cf8d8918a5668ac80ebae
snt85c <Santi.cacciola@hotmail.it>
2023-07-16 09:00:42 +0200

    BUGFIX ts and mapEvents
    seems that the compiler and ts are struggling with the newest version of
    the js google map api, so i had to do a workaround to ake sure that the eventListener for the zoom_changed is correctly registered. to do so, i create a variable to store it outside the check for coordinates (hence it's created wheter i have the coordinates or not), this way the cleaning method will always remove it from the page on unmount, so it's not an issue.

--- src/Components/BreweryItemComponents/DetailedComponents/BreweryMap.tsx
+++ src/Components/BreweryItemComponents/DetailedComponents/BreweryMap.tsx
@@ -4,57 +4,66 @@ import { Loader } from "@googlemaps/js-api-loader";
 
 export default function BreweryMap({ data }: { data: BreweryType }) {
   const ref = useRef(null);
+
   const LatLng = {
     lat: parseFloat(data.latitude),
     lng: parseFloat(data.longitude),
   };
 
   function changeZoom(map: google.maps.Map) {
+    console.log(map.getZoom());
+    console.log(map.getMapTypeId());
     if (map.getZoom()! > 15) {
-      map.setMapTypeId("satellite");
+      //high number means zooming in. Swap to satellite + markers (hybrid)
+      map.setMapTypeId("hybrid");
     } else {
-      map.setMapTypeId("roadmap");
+      //show b/w map
+      map?.setMapTypeId("roadmap");
     }
   }
 
   useEffect(() => {
-    if (data.latitude && data.longitude) {
-      let zoomListener: any;
-      let map: google.maps.Map;
-      const loader = new Loader({
-        apiKey: "AIzaSyDUGBBGWpyVDCNkHfuk4zWZII_cMCg-dtI",
-        version: "weekly",
-      });
-      loader
-        .importLibrary("maps")
-        .then(() => {
-          map = new google.maps.Map(document.getElementById("map")!, {
-            center: LatLng,
-            mapId: "ea6a8cce6ff39ab8",
-            zoom: 15,
-            gestureHandling: "greedy",
+    let zoomListener: google.maps.MapsEventListener | null = null;
+    if (data.longitude && data.latitude) {
+      if (data.latitude && data.longitude) {
+        const loader = new Loader({
+          apiKey: "AIzaSyDUGBBGWpyVDCNkHfuk4zWZII_cMCg-dtI",
+          version: "weekly",
+        });
+        loader
+          .importLibrary("maps")
+          .then(() => {
+            let map = new google.maps.Map(document.getElementById("map")!, {
+              center: LatLng,
+              mapId: "ea6a8cce6ff39ab8",
+              zoom: 15,
+              gestureHandling: "greedy",
 
-            minZoom: 10,
-            zoomControl: false, //allow zoom + and - buttons
-            mapTypeControl: false, //allows switch between map style
-            streetViewControl: false, //allows streetView
-            fullscreenControl: false, //allows see in full screen
-            keyboardShortcuts: false, //allows a tag for keyboards
-          });
+              minZoom: 10,
+              zoomControl: false, //allow zoom + and - buttons
+              mapTypeControl: false, //allows switch between map style
+              streetViewControl: false, //allows streetView
+              fullscreenControl: false, //allows see in full screen
+              keyboardShortcuts: false, //allows a tag for keyboards
+            });
 
-          new google.maps.Marker({
-            position: LatLng,
-            map,
-            title: data.name,
-          });
-          if (map)
+            new google.maps.Marker({
+              position: LatLng,
+              map,
+              title: data.name,
+            });
             zoomListener = map.addListener("zoom_changed", () =>
               changeZoom(map)
             );
-        })
-        .catch((error) => console.log(error));
-      return () => google.maps.event.removeListener(zoomListener);
+          })
+          .catch((error) => console.log(error));
+      }
     }
+    return () => {
+      if (zoomListener) {
+        google.maps.event.removeListener(zoomListener);
+      }
+    };
   }, []);
   return <div ref={ref} id="map" className="h-[50vh] w-full z-[100]"></div>;
 }
