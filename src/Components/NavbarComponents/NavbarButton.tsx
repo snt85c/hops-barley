a6620bb8b9e9de2dc467576c95e5ab292ecb1beb
snt85c <Santi.cacciola@hotmail.it>
2023-07-17 17:20:30 +0200

    Adds more to NavbarButton
    this needs a pathname so that it could resolve the change of color as well as the actual navigation. this is now implemented and working as intended

--- src/Components/NavbarComponents/NavbarButton.tsx
+++ src/Components/NavbarComponents/NavbarButton.tsx
@@ -1,12 +1,18 @@
 import { useLocation, useNavigate } from "react-router-dom";
 
-export default function NavbarButton({ text }: { text: string }) {
+export default function NavbarButton({
+  text,
+  pathname,
+}: {
+  text: string;
+  pathname: string;
+}) {
   const navigate = useNavigate();
   const location = useLocation();
   return (
     <button
-      style={{ color: location.pathname === "/" ? "black" : "" }}
-      onClick={() => navigate("")}
+      style={{ color: location.pathname === pathname ? "black" : "" }}
+      onClick={() => navigate(pathname)}
     >
       {text}
     </button>
