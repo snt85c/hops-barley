c68021c93615d0240864c4ed8e2b63c978f7f734
snt85c <Santi.cacciola@hotmail.it>
2023-07-17 17:36:19 +0200

    adds a light blur outside Overlay
    this is to make sure that the attention is kept on the overlay, as the
    background can be distracting. On click it will close the overlay as well, but the ref system has been kept since the user might click on the navbar

--- src/Components/BreweryItemComponents/DetailedComponents/Overlay.tsx
+++ src/Components/BreweryItemComponents/DetailedComponents/Overlay.tsx
@@ -18,22 +18,33 @@ export default function Overlay({
   setIsClicked: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   return (
-    <motion.div
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
-      transition={{ type: "spring", duration: 0.5 }}
-      className="fixed top-[13vh] left-[2rem] md:left-[20vw] right-[2rem] md:right-[20vw] bottom-[2rem] bg-gradient-to-b from-neutral-300 via-white to-white border-2 rounded-lg p-5 md:px-10 z-50 flex flex-col justify-between border-neutral-400"
-    >
-      <DetailedInfo data={data} />
-      <BreweryMap data={data} />
-      <div className="flex justify-center items-center">
-        <button
-          onClick={() => setIsClicked(false)}
-          className="px-5 border-2 border-neutral-500 bg-white rounded-xl w-[50vw] md:w-[10vw] mt-1"
-        >
-          close
-        </button>
-      </div>
-    </motion.div>
+    <>
+      {/* Blur overlay */}
+      <div
+        className="fixed top-0 left-0 right-0 bottom-0 bg-gray-900 bg-opacity-50 backdrop-filter backdrop-blur-sm z-40"
+        onClick={() => setIsClicked(false)}
+      ></div>
+
+      {/* Main content */}
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        className="fixed top-[13vh] left-[2rem] md:left-[20vw] right-[2rem] md:right-[20vw] bottom-[2rem] z-50 flex flex-col justify-between"
+      >
+        <div className="bg-gradient-to-b from-neutral-300 via-white to-white border-2 rounded-lg p-5 md:px-10">
+          <DetailedInfo data={data} />
+          <BreweryMap data={data} />
+          <div className="flex justify-center items-center">
+            <button
+              onClick={() => setIsClicked(false)}
+              className="px-5 border-2 border-neutral-500 bg-white rounded-xl w-[50vw] md:w-[10vw] mt-1"
+            >
+              close
+            </button>
+          </div>
+        </div>
+      </motion.div>
+    </>
   );
 }
